f69ed4218fcfbf28deba21a035b653cdfb0bc275
unknown <gabefynn15@gmail.com>
2026-02-08 01:15:05 -0500

    Adjusted the cache and created a server prior to creating the app then specified the created server to be the one triggered on deployment.

--- Yfinance_Dash_2_5.py
+++ Yfinance_Dash_2_5.py
@@ -2,33 +2,22 @@ import dash
 from dash import html, dcc, callback, Input, Output, State
 import dash_bootstrap_components as dbc
 from dash_bootstrap_templates import load_figure_template
+from flask import Flask
 from flask_caching import Cache
+# 1. Initialize Flask Server FIRST
+server = Flask(__name__)
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SIMPLEX], use_pages=True, pages_folder="pages")
-# --- SETUP FILESYSTEM CACHE ---
-cache = Cache(app.server, config={
-    # Use the file system to store cache data
+# 2. Initialize Cache using the Flask server
+# We do this BEFORE Dash so 'cache' exists when the pages try to import it
+cache = Cache(server, config={
     'CACHE_TYPE': 'FileSystemCache',
-    
-    # The folder where cache files will be saved
     'CACHE_DIR': 'cache_directory',
-    
-    # Default timeout (5 minutes). 
-    # Data older than this is automatically deleted.
     'CACHE_DEFAULT_TIMEOUT': 300,
-    
-    # Maximum number of items in the cache (prevents filling up the disk)
     'CACHE_THRESHOLD': 500
 })
-server=app.server
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SIMPLEX], use_pages=True, pages_folder="pages", server=server)
 load_figure_template('simplex')
-
-# hand-shake for the cache
-cache.init_app(app.server, config={
-    'CACHE_TYPE': 'FileSystemCache',
-    'CACHE_DIR': 'cache-directory',
-})
-
 # ONLY shared layout elements stay here
 app.layout = dbc.Container([
     dbc.Row([
